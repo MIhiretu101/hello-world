b9a0636eafbce9c75707f2673c3325664537c08d
Mihiretu1001 <mihiretu.addisu@gmail.com>
2023-05-29 10:09:51 -0400

    control flow and loop feature added
    utilized control flow and loop features to create a simple program

--- HellloWorld.java
+++ HellloWorld.java
@@ -1,12 +1,34 @@
+import java.util.*;
 public class HellloWorld{
    public static void main(String[] args){
-      System.out.println("Hello World!");
-      String name = " How to use GitHub by Prof. Noah";
+      Scanner in = new Scanner(System.in);
+      String name = "Hello World Java Edition";
+     
       System.out.println(name);
       
       for(int i = 0 ; i < name.length() ; i++){
-      
-       System.out.print("=");
+      System.out.print("=");
+       }
+       
+       System.out.println();
+       
+       char choice;
+       
+       do{
+       char answer = 'K';
+       System.out.println("I have a letter in my mind; can you guess it?");
+       String guess = in.nextLine();
+       answer = guess.toUpperCase().charAt(0);
+       if(answer == 'K'){
+       System.out.println("Your guess was damn right!");
+       }
+       else{
+       System.out.println("your guess was wrong!");
+       
+       }
+       System.out.println("Would you like to guess again? (Y/N)");
+       String response = in.nextLine();
+       choice = response.toUpperCase().charAt(0);
+       }while(choice == 'Y');
       }
    }
-}
