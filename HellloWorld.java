f31ea34298d9e6347eb1e2f0da9e7fa8cf8182e3
Mihiretu1001 <mihiretu.addisu@gmail.com>
2023-05-29 09:37:44 -0400

    output statement added
    i added some output statements

--- HellloWorld.java
+++ HellloWorld.java
@@ -1,5 +1,12 @@
 public class HellloWorld{
    public static void main(String[] args){
       System.out.println("Hello World!");
+      String name = " How to use GitHub by Prof. Noah";
+      System.out.println(name);
+      
+      for(int i = 0 ; i < name.length() ; i++){
+      
+       System.out.print("=");
+      }
    }
 }
